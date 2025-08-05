300426c76408b42f4c73a4b76281a3c0917268af
Ali Can Yücel <yucelalicandan@hotmail.com>
2025-08-05 14:29:24 +0300

    esktra componentler eklendii

---
+++ src/app/components/group/add-group/add-group.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-group',
+  imports: [],
+  templateUrl: './add-group.component.html',
+  styleUrl: './add-group.component.css'
+})
+export class AddGroupComponent {
+
+}
