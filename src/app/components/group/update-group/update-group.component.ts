99905e0befbf2bc5c614084cba8fa4cf33950a0f
Ali Can Yücel <yucelalicandan@hotmail.com>
2025-08-05 16:19:34 +0300

    ...

---
+++ src/app/components/group/update-group/update-group.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-group',
+  imports: [],
+  templateUrl: './update-group.component.html',
+  styleUrl: './update-group.component.css'
+})
+export class UpdateGroupComponent {
+
+}
